bd7d5bb1aed3d24ec42a435092c5a13d9e20e310
Wesly Goris <automationnow12@gmail.com>
2023-04-08 18:51:48 -0400

    new books

--- SeleniumAutomation/src/test/kotlin/Hello.kt
+++ SeleniumAutomation/src/test/kotlin/Hello.kt
@@ -1,31 +1,36 @@
-import org.openqa.selenium.By
 import org.openqa.selenium.WebDriver
 import org.openqa.selenium.chrome.ChromeDriver
 import org.openqa.selenium.chrome.ChromeOptions
-import org.openqa.selenium.support.ui.ExpectedConditions
-import org.openqa.selenium.support.ui.WebDriverWait
+import org.testng.Assert
+import org.testng.annotations.AfterTest
+import org.testng.annotations.BeforeTest
 import org.testng.annotations.Test
-import java.time.Duration
-
 
 class Hello {
 
-    @Test
-    fun openingPage() {
-        System.getProperty("webdriver.chrome.driver", "chromedriver.exe")
+    private var driver: WebDriver? = null
 
+    @BeforeTest
+    fun createDriver() {
+
+        System.getProperty("webdriver.chrome.driver", "chromedriver.exe")
         val options = ChromeOptions()
         options.addArguments("--remote-allow-origins=*")
-        val driver: WebDriver = ChromeDriver(options)
-        driver.get("https://Google.com")
-
-
-        val element = WebDriverWait(driver, Duration.ofSeconds(20))
-                .until(ExpectedConditions.presenceOfElementLocated(By.className("gLFyf")))
-
-        element.sendKeys("bananas")
+        driver = ChromeDriver(options)
+    }
+    @AfterTest
+    fun tearDownDriver() {
+        driver!!.quit()
     }
 
 
+    @Test
+    fun openWebPage() {
 
+    val url = "https://www.google.com/"
+   driver!!.get(url)
+   Assert.assertEquals(driver!!.currentUrl, url, "Web page url did not match expected value.")
+
+
+    }
 }
